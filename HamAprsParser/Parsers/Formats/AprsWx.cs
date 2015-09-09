1886b24ac29b97eaab2d363ecb8177fa33a5320f
Jonathan Bradshaw <jonathan@nrgup.net>
2015-09-09 12:00:04 -0500

    Cleanup weather parsing class

--- HamAprsParser/Parsers/Formats/AprsWx.cs
+++ HamAprsParser/Parsers/Formats/AprsWx.cs
@@ -22,6 +22,19 @@ namespace HamAprsParser.Parsers.Formats
     internal static class AprsWx
     {
 
+        #region Private Fields
+
+        private static readonly Regex CleanupRegex = new Regex(@".*[gtrPplLs#hbVv][0-9\+\-\._ ]{2,5}");
+        private static readonly Regex HumidityRegex = new Regex(@"h(?<humidity>[0-9]{1,3})", RegexOptions.ExplicitCapture);
+        private static readonly Regex LuminosityRegex = new Regex(@"h(?<luminosity>[0-9]{1,3})", RegexOptions.ExplicitCapture);
+        private static readonly Regex PressureRegex = new Regex(@"b(?<pressure>\d{4,5})", RegexOptions.ExplicitCapture);
+        private static readonly Regex Rain1HRegex = new Regex(@"r(?<rain>[0-9]{1,3})", RegexOptions.ExplicitCapture);
+        private static readonly Regex Rain24HRegex = new Regex(@"p(?<rain>[0-9]{1,3})", RegexOptions.ExplicitCapture);
+        private static readonly Regex RainTodayRegex = new Regex(@"P(?<rain>[0-9]{1,3})", RegexOptions.ExplicitCapture);
+        private static readonly Regex SnowfallRegex = new Regex(@"[gt][0-9]{1,3}.*s(?<snowfall>[0-9]{1,3})", RegexOptions.ExplicitCapture);
+
+        #endregion Private Fields
+
         #region Public Methods
 
         /// <summary>
@@ -31,13 +44,19 @@ namespace HamAprsParser.Parsers.Formats
         /// <returns></returns>
         public static string GetComment(string payload) => Clean(payload).Trim();
 
+        /// <summary>
+        /// Parses the humidity.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        /// <param name="humidity">The humidity.</param>
+        /// <returns>System.Boolean.</returns>
         public static bool ParseHumidity(
-                    string data,
-            out int? humidity)
+                            string data,
+                    out int? humidity)
         {
             humidity = null;
 
-            var match = Regex.Match(data, @"h(?<humidity>\d{1,3})", RegexOptions.ExplicitCapture);
+            var match = HumidityRegex.Match(data);
             if (match.Success)
             {
                 humidity = int.Parse(match.Groups["humidity"].Value);
@@ -49,13 +68,19 @@ namespace HamAprsParser.Parsers.Formats
             return false;
         }
 
+        /// <summary>
+        /// Parses the luminosity.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        /// <param name="luminosity">The luminosity.</param>
+        /// <returns>System.Boolean.</returns>
         public static bool ParseLuminosity(
-            string data,
-            out int? luminosity)
+                    string data,
+                    out int? luminosity)
         {
             luminosity = null;
 
-            var match = Regex.Match(data, @"[lL](?<luminosity>\d{1,3})", RegexOptions.ExplicitCapture);
+            var match = LuminosityRegex.Match(data);
             if (match.Success)
             {
                 luminosity = int.Parse(match.Groups["luminosity"].Value);
@@ -66,13 +91,19 @@ namespace HamAprsParser.Parsers.Formats
             return false;
         }
 
+        /// <summary>
+        /// Parses the pressure.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        /// <param name="pressure">The pressure.</param>
+        /// <returns>System.Boolean.</returns>
         public static bool ParsePressure(
-            string data,
-            out int? pressure)
+                    string data,
+                    out int? pressure)
         {
             pressure = null;
 
-            var match = Regex.Match(data, @"b(?<pressure>\d{4,5})", RegexOptions.ExplicitCapture);
+            var match = PressureRegex.Match(data);
             if (match.Success)
             {
                 pressure = int.Parse(match.Groups["pressure"].Value);
@@ -82,36 +113,50 @@ namespace HamAprsParser.Parsers.Formats
             return false;
         }
 
+        /// <summary>
+        /// Parses the rain.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        /// <param name="rainInch1H">The rain inch1 h.</param>
+        /// <param name="rainInch24H">The rain inch24 h.</param>
+        /// <param name="rainInchMidnight">The rain inch midnight.</param>
+        /// <returns>System.Boolean.</returns>
         public static bool ParseRain(
-            string data,
-            out float? rainInch1H,
-            out float? rainInch24H,
-            out float? rainInchMidnight)
+                    string data,
+                    out float? rainInch1H,
+                    out float? rainInch24H,
+                    out float? rainInchMidnight)
         {
             rainInch1H = rainInch24H = rainInchMidnight = null;
 
-            var match = Regex.Match(data, @"r(?<rain>\d{1,3})", RegexOptions.ExplicitCapture);
+            var match = Rain1HRegex.Match(data);
             if (match.Success)
                 rainInch1H = int.Parse(match.Groups["rain"].Value);
 
-            match = Regex.Match(data, @"p(?<rain>\d{1,3})", RegexOptions.ExplicitCapture);
+            match = Rain24HRegex.Match(data);
             if (match.Success)
                 rainInch24H = int.Parse(match.Groups["rain"].Value);
 
-            match = Regex.Match(data, @"P(?<rain>\d{1,3})", RegexOptions.ExplicitCapture);
+            match = RainTodayRegex.Match(data);
             if (match.Success)
                 rainInchMidnight = int.Parse(match.Groups["rain"].Value);
 
             return rainInch1H.HasValue || rainInch24H.HasValue || rainInchMidnight.HasValue;
         }
 
+        /// <summary>
+        /// Parses the snowfall.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        /// <param name="snowInches">The snow inches.</param>
+        /// <returns>System.Boolean.</returns>
         public static bool ParseSnowfall(
-            string data,
-            out float? snowInches)
+                    string data,
+                    out float? snowInches)
         {
             snowInches = null;
 
-            var match = Regex.Match(data, @"[gt]\d{1,3}.*s(?<snowfall>\d{1,3})", RegexOptions.ExplicitCapture);
+            var match = SnowfallRegex.Match(data);
             if (match.Success)
             {
                 snowInches = int.Parse(match.Groups["snowfall"].Value);
@@ -121,17 +166,26 @@ namespace HamAprsParser.Parsers.Formats
             return false;
         }
 
+        /// <summary>
+        /// Parses the wind and temperature.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        /// <param name="windSpeedMph">The wind speed MPH.</param>
+        /// <param name="windDirection">The wind direction.</param>
+        /// <param name="temperatureF">The temperature f.</param>
+        /// <param name="windGustMph">The wind gust MPH.</param>
+        /// <returns>System.Boolean.</returns>
         public static bool ParseWindAndTemperature(
-            string data,
-            out float? windSpeedMph,
-            out int? windDirection,
-            out float? temperatureF,
-            out float? windGustMph)
+                    string data,
+                    out float? windSpeedMph,
+                    out int? windDirection,
+                    out float? temperatureF,
+                    out float? windGustMph)
         {
             windSpeedMph = temperatureF = windGustMph = null;
             windDirection = null;
 
-            var match = Regex.Match(data, @"(?<course>[\d \.\-]{3})\/(?<speed>[\d \.]{3})g(?<gust>[\d \.]+)t(?<temp>-?[\d \.]+)", RegexOptions.ExplicitCapture);
+            var match = Regex.Match(data, @"(?<course>[0-9 \.\-]{3})\/(?<speed>[0-9 \.]{3})g(?<gust>[0-9 \.]+)t(?<temp>-?[0-9 \.]+)", RegexOptions.ExplicitCapture);
             if (match.Success)
             {
                 windDirection = Conversions.ParseInt(match.Groups["course"].Value);
@@ -145,7 +199,7 @@ namespace HamAprsParser.Parsers.Formats
                 return true;
             }
 
-            match = Regex.Match(data, @"c(?<course>[\d \.\-]{3})s(?<speed>[\d \.]{3})g(?<gust>[\d \.]+)t(?<temp>-?[\d \.]+)", RegexOptions.ExplicitCapture);
+            match = Regex.Match(data, @"c(?<course>[0-9 \.\-]{3})s(?<speed>[0-9 \.]{3})g(?<gust>[0-9 \.]+)t(?<temp>-?[0-9 \.]+)", RegexOptions.ExplicitCapture);
             if (match.Success)
             {
                 windDirection = Conversions.ParseInt(match.Groups["course"].Value);
@@ -159,7 +213,7 @@ namespace HamAprsParser.Parsers.Formats
                 return true;
             }
 
-            match = Regex.Match(data, @"(?<course>[\d \.\-]{3})\/(?<speed>[\d \.]{3})t(?<temp>-?[\d \.]+)", RegexOptions.ExplicitCapture);
+            match = Regex.Match(data, @"(?<course>[0-9 \.\-]{3})\/(?<speed>[0-9 \.]{3})t(?<temp>-?[0-9 \.]+)", RegexOptions.ExplicitCapture);
             if (match.Success)
             {
                 windDirection = Conversions.ParseInt(match.Groups["course"].Value);
@@ -171,7 +225,7 @@ namespace HamAprsParser.Parsers.Formats
                 return true;
             }
 
-            match = Regex.Match(data, @"(?<course>[\d \.\-]{3})\/(?<speed>[\d \.]{3})g(?<gust>[\d \.]+)", RegexOptions.ExplicitCapture);
+            match = Regex.Match(data, @"(?<course>[0-9 \.\-]{3})\/(?<speed>[0-9 \.]{3})g(?<gust>[0-9 \.]+)", RegexOptions.ExplicitCapture);
             if (match.Success)
             {
                 windDirection = Conversions.ParseInt(match.Groups["course"].Value);
@@ -189,8 +243,9 @@ namespace HamAprsParser.Parsers.Formats
 
         #region Private Methods
 
-        private static string Clean(string data) => Regex.Replace(data, @".*[gtrPplLs#hbVv][0-9\+\-\._ ]{2,5}", string.Empty);
+        private static string Clean(string data) => CleanupRegex.Replace(data, string.Empty);
 
         #endregion Private Methods
+
     }
 }
